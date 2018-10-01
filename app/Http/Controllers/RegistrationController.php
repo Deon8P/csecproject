b51f0238fc3f3044cbe6aa311bf72f6d0e43d51b
Deon96 <Gideondeonjacobus@Gmail.com>
2018-10-01 11:46:49 +0200

    Fixed some stuff

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -14,13 +14,12 @@ class RegistrationController extends Controller
 
     public function store()
     {
-        /*
     	$this->validate(request(), [
             'name' => 'required|min:2',
             'surname' => 'required|min:2',
-    		'email' => 'required|email|unique:users,email',
+    		'email' => 'required|email|unique:managers,email',
     		'password' => 'required|confirmed|min:6'
-    	]);*/
+    	]);
 
     	Manager::create([
             'name' => request('name'),
