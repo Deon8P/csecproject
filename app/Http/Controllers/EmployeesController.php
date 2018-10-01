766b14f99fead0b7641b89fbf2baca85c0a366bd
Brandon <brandon.lubbe08@gmail.com>
2018-10-01 18:38:44 +0200

    Store Methods

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Employee;
 
 class EmployeesController extends Controller
 {
-  //
+    public function store()
+    {
+        $this->validate(request(), [
+            'name' => 'required|min:2',
+            'surname' => 'required|min:2',
+            'email' => 'required|email|unique:managers,email',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        Employee::create([
+            'name' => request('name'),
+            'surname' => request('surname'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password'))
+        ]);
+
+        return back();
+    }
 }
--- app/Http/Controllers/LeavesController.php
+++ app/Http/Controllers/LeavesController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Leave;
 
 class LeavesController extends Controller
 {
-    //
+    public function store()
+    {
+        $this->validate(request(), [
+            'emp_id' => 'required',
+            'leave_type' => 'required',
+            'period' => 'required|email|unique:managers,email',
+            'status' => 'required|confirmed|min:6'
+        ]);
+
+        Leave::create([
+            'emp_id' => request('emp_id'),
+            'leave_type' => request('leave_type'),
+            'period' => request('period'),
+            'status' => bcrypt(request('status'))
+        ]);
+
+        return back();
+    }
 }
