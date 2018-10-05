38f183f1f589ffd8328be9038056018125e3355e
Deon96 <Gideondeonjacobus@Gmail.com>
2018-10-05 10:59:34 +0200

    added admins

---
+++ database/migrations/2018_10_01_081129_create_admins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_username');
+            $table->string('name');
+            $table->string('surname');
+            $table->timestamps();
+        });
+
+        Schema::table('admins', function(Blueprint $table)
+        {
+            $table->foreign('user_username')->references('username')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
